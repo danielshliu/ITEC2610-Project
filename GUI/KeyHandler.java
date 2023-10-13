c74c52081b1a1643d03f7c37d7dc5627862213c9
JacobPhillips22 <147002624+JacobPhillips22@users.noreply.github.com>
2023-10-13 14:18:10 -0400

    Create KeyHandler.java

---
+++ GUI/KeyHandler.java
@@ -0,0 +1,56 @@
+
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    boolean up = false;
+    boolean down = false;
+    boolean left = false;
+    boolean right = false;
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        /*if (code == KeyEvent.VK_W){
+            up = true;
+        }
+        if (code == KeyEvent.VK_S){
+            down = true;
+        } */
+        if (code == KeyEvent.VK_A){
+            left = true;
+        }
+        if (code == KeyEvent.VK_D){
+            right = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        /*
+        if (code == KeyEvent.VK_W){
+            up = false;
+        }
+        if (code == KeyEvent.VK_S){
+            down = false;
+        } */
+
+        if (code == KeyEvent.VK_A){
+            left = false;
+        }
+        if (code == KeyEvent.VK_D){
+            right = false;
+        }
+    }
+}
