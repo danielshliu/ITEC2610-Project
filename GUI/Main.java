bae91fa93a0f79d7e6cabf4e0240c5aa5ae855cc
Teaapoo <dliu224@my.yorku.ca>
2023-11-10 18:38:45 -0500

    Working on Front end stuff

--- GUI/Main.java
+++ GUI/Main.java
@@ -4,18 +4,37 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        JButton start = new JButton("Start");
+        JButton options = new JButton("Options");
+        JButton tutorial = new JButton("Tutorial");
+        JButton exit = new JButton("Exit");
+
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Game Window");
+        window.setTitle("Asteroid Invaders");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
+
 
         window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        gamePanel.startGameThread();
+
+        //This starts the game.
+
+        //gamePanel.startGameThread();
+
+    }
+    private void startMenu(){
+
+    }
+    private void optionsMenu(){
+
+    }
+    private void tutorial(){
 
     }
 }
--- GUI/MainFrame.java
+++ GUI/MainFrame.java
@@ -1,11 +1,48 @@
 package GUI;
 
+import javax.swing.*;
 import java.awt.*;
-
-public class MainFrame{
-    
+import java.awt.event.*;
 
 
+public class MainFrame implements ItemListener {
+    JPanel Cards;
+    final static String startButton = "Start Menu!";
+    final static String optionMenu = "Options!";
+    final static String tutorialMenu = "Tutorial!";
 
-    
+    public void addComponentToCard(Container card){
+        JPanel tutorialCard  = new JPanel();
+        tutorialCard.add(new JTextArea("This is a Tutorial"));
+
+        JPanel startMenu = new JPanel();
+
+
+        JPanel options = new JPanel();
+        options.add(new JSlider(0,100,50));
+
+    }
+
+    public void itemStateChanged(ItemEvent event){
+        CardLayout cardlayout = (CardLayout)(Cards.getLayout());
+        cardlayout.show(Cards, (String)event.getItem());
+
+    }
+
+    private static void generateAndShowGUI(){
+        JFrame frame = new JFrame("Testing out stuff");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MainFrame main = new MainFrame();
+        main.addComponentToCard(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
