9607e66c4403b7b3de545d2f6d70df0a253d3e71
JacobPhillips22 <147002624+JacobPhillips22@users.noreply.github.com>
2023-11-26 18:29:01 -0500

    Revert "Asteriod text in bg"
    This reverts commit 379b302f483db861d70c34b4f6e97edd698ca21d.

--- Game/Game.java
+++ Game/Game.java
@@ -45,8 +45,6 @@ public class Game extends JPanel implements Runnable{
 
 
     public ArrayList<Enemy1> asteroids = new ArrayList<>();
-    int enemyCount;
-    Font stringFont = new Font( "SansSerif", Font.BOLD, 60 );
     ArrayList<Enemy1> enemies = new ArrayList<>();
     ArrayList<Bullet> bullets = new ArrayList<>();
 
@@ -170,7 +168,6 @@ public class Game extends JPanel implements Runnable{
                 enemies.add(e);
             }
         }
-        enemyCount = enemies.size();
 
         //Change character back to testCharacter if error
         for (int o = 0; o < testCharacter.bulletInChamber.size(); o++){
@@ -212,13 +209,6 @@ public class Game extends JPanel implements Runnable{
 
         // Enemy (Asteroid)
         Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.gray);
-        g2.setFont(stringFont);
-        g2.drawString(""+enemyCount, screenWidth/2, screenHeight/2);
-        g2.setColor(Color.white);
-        testCharacter.drawHitbox(g2);
-
         for (Enemy1 e : asteroids){
             if (e.valid){
                 e.draw(g2);
@@ -229,6 +219,7 @@ public class Game extends JPanel implements Runnable{
         // User (Paint last)
         testCharacter.draw(g2);
 
+        testCharacter.drawHitbox(g2);
         g2.dispose();
     }
 
